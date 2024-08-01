8500c23981de31c26ef1604c750ac5e30d8b8185
siddhant-kumar786 <siddhantkumar100d@gmail.com>
2024-08-02 00:36:21 +0530

    adding different ringtones and completed the alarm part of this website

--- script.js
+++ script.js
@@ -2,37 +2,46 @@ let string = "";
 let dateString = "";
 let showTime = document.getElementById("showTime");
 let showDate = document.getElementById("showDate");
-let m = new Audio("alarm.mp3");
+let m;
+
 // m.play();
+
+// show date and time on the screen
 setInterval(() => {
     d = new Date();
     htime = d.getHours();
     day = d.getDate();
-    dateString+=day;
-    dateString+="/";
-    string+=htime;
-    string+=": " 
+    dateString += day;
+    dateString += "/";
+    string += htime;
+    string += ": "
     mtime = d.getMinutes();
     months = d.getMonth();
-    dateString+=months;
-    dateString+="/";
+    dateString += months;
+    dateString += "/";
 
-    string+=mtime;
-    string+=": ";
+    string += mtime;
+    string += ": ";
     stime = d.getSeconds();
     year = d.getFullYear();
-    dateString+=year;
-    string+=stime;
+    dateString += year;
+    string += stime;
     showTime.innerHTML = string;
     showDate.innerHTML = dateString;
     string = "";
-    dateString ="";
+    dateString = "";
 
 
 }, 1000);
 
+//select the song
 
-// Add or remove alarm
+
+
+
+
+
+//Event listeners
 let setAlarm = document.getElementById('setalarm');
 let fixedAlarm = document.getElementById('set');
 let content = document.getElementById('content');
@@ -43,91 +52,84 @@ let stopAlarmbtn = document.getElementById('stop');
 let mode = document.querySelector('.mode');
 
 let navbar = document.querySelector('nav');
-setAlarm.addEventListener('click', ()=>{
-    // content.style.display = "none";
+setAlarm.addEventListener('click', () => {
     content.classList.add('opacityset');
-    // content.style.backgroundColor = "black";
     setAlarmOption.style.display = "flex";
     setAlarm.style.display = "none";
-    // setAlarmOption.classList.toggle('animate');
-    
 })
 
-fixedAlarm.addEventListener('click', ()=>{
+fixedAlarm.addEventListener('click', () => {
     content.style.display = "flex";
     setAlarmOption.style.display = "none";
-    afterAlarmSet.style.display ="flex";
+    afterAlarmSet.style.display = "flex";
     setAlarm.style.display = "none";
     content.style.backgroundColor = "#4e4e54";
     content.classList.remove('opacityset');
     getOptions();
-    matchingAlarmVsRealTime();
 })
 
-stopAlarmbtn.addEventListener('click', ()=>{
+stopAlarmbtn.addEventListener('click', () => {
     afterAlarmSet.style.display = "none";
     content.style.backgroundColor = "black";
     setAlarm.style.display = "flex";
+    clearInterval(timerId);
+    m.pause();
 })
-mode.addEventListener('click', ()=>{
-    if(navbar.style.backgroundColor = "4e4e4e")
-    {
+let turn = 0;
+mode.addEventListener('click', () => {
+
+    if (turn == 0) {
         navbar.style.backgroundColor = "#007abc";
         content.style.backgroundColor = "white";
+        turn = 1;
     }
-    else{
-        navbar.style.backgroundColor = "4e4e4e";
+    else {
+        navbar.style.backgroundColor = "#4e4e4e";
         content.style.backgroundColor = "black";
+        turn = 0;
     }
 })
 
 let settimeAlarm = document.querySelector('.setTime');
 let selectHours = document.querySelector('#Hours');
 let selectMinutes = document.querySelector('#Minutes');
-let selectAmPm = document.querySelector('#ampm');
-let output, output1, output2;
-function getOptions()
-{
-    
-            output = selectHours.value;
-            console.log(output);
-            // console.log(typeof(output));
-            settimeAlarm.innerHTML = output;
-            settimeAlarm.innerHTML += ': '
+let output, output1;
+function getOptions() {
+    output = selectHours.value;
+    settimeAlarm.innerHTML = output;
+    settimeAlarm.innerHTML += ': '
 
-            
-            output1 = selectMinutes.value;
-            settimeAlarm.innerHTML += output1;
-            settimeAlarm.innerHTML += " ";
 
-            
-            output2 = selectAmPm.value;
-            settimeAlarm.innerHTML+= output2;
-}
-// let newNum = 0;
-function matchingAlarmVsRealTime()
-{
-    if(output2 === "AM" && output === "12")
-    {
-        output = "0";
-    }
-    if(output2 === "PM")
-    {
-        let count = 12;
-        let newNum = Number.parseInt(output);
-        if(newNum != 12)
-            output = count + newNum;
-        else 
-            output = count;
-    }
-    console.log(output);
-    console.log(output2);
+    output1 = selectMinutes.value;
+    settimeAlarm.innerHTML += output1;
+    settimeAlarm.innerHTML += " ";
+
+    let songName ="alarm";
+    let song = document.getElementById('song');
+    songName+=song.value;
+    m = new Audio(songName + ".mp3");
 }
 
+// alarm starts if the time completed
+let alarmRing = false;
+function playAlarmifTimeEnd() {
+    let d = new Date();
+    if ((output == d.getHours() && output1 == d.getMinutes()) || alarmRing) {
+        alarmRing = true;
+        m.play();
+    }
+}
 
 let timerId = setInterval(() => {
-    m.play();
+    playAlarmifTimeEnd();  
 }, 1000);
+
+
+
+
+
+
+
 
 
 
